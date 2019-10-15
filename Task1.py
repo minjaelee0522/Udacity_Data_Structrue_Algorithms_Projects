4f997dc8788567b96fb50c1bd6ca0947341ac2ed
minjae <32402820+kkrlee@users.noreply.github.com>
2019-10-14 22:28:38 -0400

    revise txt

--- Task1.py
+++ Task1.py
@@ -12,23 +12,19 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-numbers1 = []
-numbers2 = []
+numbers = set()
+
 for number in texts:
-    numbers1.append(number[0])
-    numbers2.append(number[1])
+    numbers.add(number[0])
+    numbers.add(number[1])
 
-
-numbers3 = []
-numbers4 = []
 for number in calls:
-    numbers3.append(number[0])
-    numbers4.append(number[1])
+    numbers.add(number[0])
+    numbers.add(number[1])
 
-numbers = set(numbers1 + numbers2 + numbers3 + numbers4)
+answer = len(numbers)
 
-print("There are {0} different telephone numbers in the records.".format(len(numbers)))
-
+print("There are {0} different telephone numbers in the records.".format(answer))
 """
 TASK 1:
 How many different telephone numbers are there in the records? 
